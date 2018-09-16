a6950a00cccde256c7f5f5add492c783cb8f9509
Koki Takahashi <hakatasiloving@gmail.com>
2018-09-16 11:32:36 +0900

    Revert "はやくする"
    This reverts commit dd6797e4522e405b71ae6cadf4b336b4e0750042.

--- webapp/ruby/lib/torb/web.rb
+++ webapp/ruby/lib/torb/web.rb
@@ -58,9 +58,9 @@ module Torb
 
         db.query('BEGIN')
         begin
-          raw_events = db.query('SELECT * FROM events ORDER BY id ASC').select(&where)
-          events = raw_events.map do |raw_event|
-            event = get_event(raw_event)
+          event_ids = db.query('SELECT * FROM events ORDER BY id ASC').select(&where).map { |e| e['id'] }
+          events = event_ids.map do |event_id|
+            event = get_event(event_id)
             event['sheets'].each { |sheet| sheet.delete('detail') }
             event
           end
@@ -72,7 +72,10 @@ module Torb
         events
       end
 
-      def get_event(event, login_user_id = nil)
+      def get_event(event_id, login_user_id = nil)
+        event = db.xquery('SELECT * FROM events WHERE id = ?', event_id).first
+        return unless event
+
         # zero fill
         event['total']   = 0
         event['remains'] = 0
